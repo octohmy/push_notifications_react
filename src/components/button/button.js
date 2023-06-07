a92aeae03fe5da29779a5e91228f3f0048c94425
大傻逼 <m.hawes93@gmail.com>
2023-06-07 18:09:16 +0100

    button with timer

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,22 +1,47 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 function Button() {
+  const buttonRef = useRef(null);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (buttonRef.current) {
+        buttonRef.current.click();
+      }
+    }, 5000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
   const handleClick = () => {
     if ("Notification" in window && Notification.permission === "granted") {
-      new Notification("You have a new message!");
-    } else if (
-      "Notification" in window &&
-      Notification.permission !== "denied"
-    ) {
+      const notification = new Notification("You have a new message!", {
+        body: "Hello",
+      });
+
+      notification.addEventListener("click", function (event) {
+        window.open("https://www.google.com", "_blank");
+      });
+    } else if ("Notification" in window && Notification.permission !== "denied") {
       Notification.requestPermission().then((permission) => {
         if (permission === "granted") {
-          new Notification("You have a new message!");
+          const notification = new Notification("You have a new message!");
+
+          notification.addEventListener("click", function (event) {
+            window.open("https://www.google.com", "_blank");
+          });
         }
       });
     }
   };
 
-  return <button onClick={handleClick}>Click here for push test!</button>;
+  return (
+    <button ref={buttonRef} onClick={handleClick}>
+      Click here for push test!<br></br>I will auto press after 5 seconds
+    </button>
+  );
 }
 
 export default Button;
