ab284f10e3977004498a2cc5f07ebb0b799145d5
大傻逼 <m.hawes93@gmail.com>
2023-06-13 10:57:47 +0100

    try again

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -3,17 +3,36 @@ import logoIcon from "./logo2.png";
 
 function Button() {
   const buttonRef = useRef(null);
+  const timer = useRef(null);
+  const awayTimer = useRef(null);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
+    timer.current = setTimeout(() => {
       if (buttonRef.current) {
         buttonRef.current.click();
       }
     }, 5000);
 
     return () => {
-      clearTimeout(timer);
+      clearTimeout(timer.current);
     };
+  }, []);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const handleVisibilityChange = () => {
+        if (document.hidden) {
+          awayTimer.current = setInterval(() => {
+            createNotification("Reminder", "You've been away for 10 seconds, please get back to work!");
+          }, 10000);  // 10 seconds reminder
+        } else {
+          clearInterval(awayTimer.current);
+        }
+      };
+
+      document.addEventListener('visibilitychange', handleVisibilityChange);
+      return () => document.removeEventListener('visibilitychange', handleVisibilityChange);
+    }
   }, []);
 
   const createNotification = (title, body) => {
